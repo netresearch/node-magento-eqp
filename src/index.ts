6d6d3099ee1afe1c9a01d7704b2d1e4a59beee8a
TheDevMinerTV <tobigames200@gmail.com>
2021-03-31 16:32:18 +0200

    Add files

---
+++ src/index.ts
@@ -0,0 +1,150 @@
+import Axios, { AxiosInstance } from 'axios';
+import { Environment, Magento1Key, Magento2Key, Options, Package, User, UserSummary } from './types';
+
+export class EQP {
+	protected environment: Environment;
+	protected autoRefreshToken: boolean;
+
+	protected client: AxiosInstance;
+
+	mageId?: string;
+
+	constructor(options?: Partial<Options>) {
+		this.environment = options?.environment ?? 'production';
+		this.autoRefreshToken = options?.autoRefresh ?? false;
+
+		this.client = Axios.create({
+			baseURL: `https://developer${this.environment === 'staging' ? '-stg' : ''}-api.magento.com/rest/v1`
+		});
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/auth.html#session-token */
+	async authenticate(clientId: string, clientSecret: string, expiresIn?: number): Promise<void> {
+		const {
+			data: { expires_in, mage_id, ust }
+		} = await this.client.post<{ mage_id: string; ust: string; expires_in: number }>(
+			'/app/session/token',
+			{ grant_type: 'session', expires_in: expiresIn ?? 7200 },
+			{
+				auth: {
+					password: clientSecret,
+					username: clientId
+				}
+			}
+		);
+
+		this.mageId = mage_id;
+
+		this.client.defaults.headers.common['Authorization'] = `Bearer ${ust}`;
+
+		if (this.autoRefreshToken) {
+			setTimeout(() => this.authenticate(clientId, clientSecret), expires_in * 1000);
+		}
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/auth.html */
+	getUser(): Promise<User>;
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/auth.html */
+	getUser(summary?: boolean): Promise<UserSummary>;
+
+	async getUser(summary?: boolean): Promise<User | UserSummary> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.get(`/users/${this.mageId}${summary ? '?style=summary' : ''}`)).data;
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/auth.html#session-token */
+	async updateUser(mageId: string, data: Partial<User & { action: 'submit' }>): Promise<Package[]> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.put(`/users/${mageId}`, { action: 'submit', ...data })).data;
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/users.html#get-keys */
+	async getKeys(
+		options?: Partial<{ type: 'all' | 'm1' | 'm2'; label: string }>
+	): Promise<{ m1: Magento1Key[]; m2: Magento2Key[] }> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (
+			await this.client.get(`/users/${this.mageId}/keys`, {
+				params: {
+					type: options?.type,
+					label: options?.label
+				}
+			})
+		).data;
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/users.html#user-reports */
+	async getPageviewReports(): Promise<unknown> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.get(`/users/${this.mageId}/reports/pageviews`)).data;
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/users.html#user-reports */
+	async getTotalReports(): Promise<unknown> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.get(`/users/${this.mageId}/reports/totals`)).data;
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/users.html#user-reports */
+	async getSalesReports(): Promise<unknown> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.get(`/users/${this.mageId}/reports/pageviews`)).data;
+	}
+
+	/** @see https://devdocs.magento.com/marketplace/eqp/v1/users.html#user-reports */
+	async getRefundReports(): Promise<unknown> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.get(`/users/${this.mageId}/reports/refunds`)).data;
+	}
+
+	async getPackages(): Promise<Package[]> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.get('/products/packages')).data;
+	}
+
+	async getReports(metricName?: string): Promise<unknown> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return (await this.client.get(`/reports/metrics/${metricName ?? ''}`)).data;
+	}
+
+	async registerCallback(name: string, url: string, username: string, password: string): Promise<Package[]> {
+		if (!this.mageId) {
+			throw new Error('Not authenticated.');
+		}
+
+		return this.updateUser(this.mageId, {
+			api_callbacks: [{ name, url, username, password }]
+		});
+	}
+
+	// TODO
+	// parseCallback(event: CallbackEvent): Promise<unknown>;
+}
