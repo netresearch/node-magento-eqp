853158259a53f1ec850009917827ef7ba4b6bd92
TheDevMinerTV <tobigames200@gmail.com>
2021-04-06 11:13:56 +0200

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+// ESLint, thank you for linting your own file and breaking it
+// eslint-disable-next-line no-undef
+module.exports = {
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended']
+};
