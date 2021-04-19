32dfe98c1517ccc80200bac0817c1923f7b42e75
Paul Siedler <paul.siedler@netresearch.de>
2021-04-19 14:35:52 +0000

    NRFE-3739: reformat files according to editorconfig, document recommended/required extensions

--- tests/MockAdapter.ts
+++ tests/MockAdapter.ts
@@ -2,9 +2,11 @@ import MockAdapter from "axios-mock-adapter";
 import { AxiosAdapter } from "../src/Adapters";
 
 export class AxiosMockAdapter extends AxiosAdapter {
-    mockAdapter: MockAdapter
-    constructor(baseURL: string) {
-        super(baseURL);
-        this.mockAdapter = new MockAdapter(this.client, { onNoMatch: 'throwException' });
-    }
+	mockAdapter: MockAdapter;
+	constructor(baseURL: string) {
+		super(baseURL);
+		this.mockAdapter = new MockAdapter(this.client, {
+			onNoMatch: "throwException",
+		});
+	}
 }
